a38da2f1dd0920b943ca5988920b36eec634c60c
cexyzp <arissuryana301@gmail.com>
2026-02-14 12:42:48 +0700

    Oke Gas

--- app.js
+++ app.js
@@ -1,22 +1,27 @@
 const express = require("express");
-const app = express();
-const PORT = 3000;
+const path = require("path");
 
+const app = express();
+
+app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
+
 app.use((req, res) => {
   const key = req.query.key;
 
-  if (key === "gocep") {
+  if (key === "aris") {
     return res.render("result");
   }
-  if (key == "aris") {
+
+  if (key === "gocep") {
     return res.render("admin");
   }
-  res.render("vote");
+
+  res.render("closed");
 });
 
 module.exports = app;
